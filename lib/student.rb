c73d64be6898538752006087c86d6b22d39a6c9c
Candice Peters <candicelizabethpeters@gmail.com>
2019-10-21 21:58:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,4 +20,14 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
+  def self.drop_table
+    sql =  <<-SQL 
+      DROP TABLE students (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade INTEGER
+        )
+        SQL
+        DB[:conn].execute(sql)
+  end
 end
