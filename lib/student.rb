64024cdbe79ec5737bec23e1b79e29433d7a5141
Candice Peters <candicelizabethpeters@gmail.com>
2019-10-21 21:54:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,6 +6,17 @@ class Student
     @name = name
     @grade = grade
   end
+  
+  def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade INTEGER
+        )
+        SQL
+    DB[:conn].execute(sql) 
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
